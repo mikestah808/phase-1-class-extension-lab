0d93fd987a55bba3e476c73dda48c895a34e6197
Michael Stafford <michaelstafford808@gmail.com>
2022-02-13 11:59:00 -1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+
+class Polygon{
+    constructor(arrOfIntegers){
+        // console.log(arrOfIntegers);
+        this.arrOfIntegers = arrOfIntegers
+    }
+
+    get countSides(){
+        return this.arrOfIntegers.length;
+    }
+
+    get perimeter(){
+        // perimeter = (length + width) * 2;
+        // calculates the sum of each side (each index of the array) of the polygon
+        let sum = this.arrOfIntegers.reduce((a,b) => a + b, 0);
+        return sum;
+    }
+
+}
+
+class Triangle extends Polygon {
+
+}
+
+
+class Square extends Polygon {
+
+}
